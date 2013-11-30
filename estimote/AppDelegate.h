9da4165383eb5fceab4e45d7ea70bc0feae58c29
Jon Lee <jonlee@Macintosh.local>
2013-11-30 14:45:16 -0800

    Initial Commit

---
+++ estimote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  estimote
+//
+//  Created by Jon Lee on 11/30/13.
+//  Copyright (c) 2013 Jon Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ estimoteTests/estimoteTests.m
@@ -0,0 +1,34 @@
+//
+//  estimoteTests.m
+//  estimoteTests
+//
+//  Created by Jon Lee on 11/30/13.
+//  Copyright (c) 2013 Jon Lee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface estimoteTests : XCTestCase
+
+@end
+
+@implementation estimoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
